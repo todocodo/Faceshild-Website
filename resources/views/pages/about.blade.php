853050121a98ab9f18dff5546a72e1664f19f15f
unknown <todomodo12@gmail.com>
2020-08-07 17:50:05 +0300

    About page almost ready

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,29 +4,100 @@
 <div class="p-3 p-md-5 m-lg-3 text-center bg-light line-size">
     <div class="row">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
-            <h1 class="display-4 font-weight-normal">Faceshield</h1>
-            <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-            <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+            <h1 class="font-weight-normal" style="color: #ec9a2e">Protect the eyes nose and face</h1>
+            <br><br>
+            <p class="lead font-weight-normal">nTr Professional Faceshield guarantee the protection of the eyes, nose and face from drops and splashes of liquids, responding to the European Regulation (ЕС) 2016/425, European Standard EN166:2001 point 7.2.4 b, EN168:2008 objection 12.2 and the requirements for EN166:2001 point 6.1, 6.2, 6.3;</p>
         </div>
-        <div class="col-md-4 p-lg-3">
-            <img src="../photo/camera_noback.png" alt="camera">
+        <div class="col-md-4">
+            <img src="../photo/new_logo_nonbackground.png" alt="logo" style="height: 250px; margin-top: 65px;">
         </div>
     </div>
 </div>
 
-<div class="p-3 p-md-5 m-lg-3 text-center bg-dark line-size">
-    <div class="col-md-5 p-lg-5 mx-auto my-5 text-white">
-        <h1 class="display-4 font-weight-normal">Faceshield</h1>
-        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+<!-- GERY -->
+<div class="p-3 p-md-5 m-lg-3 bg-secondary line-size">
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <img src="../photo/orange_shoe.png" alt="shoe" style="height: 550px;">
+        </div>
+        <div class="col-md-5 p-lg-5 my-5 text-white" style="margin-left: 350px;">
+            <h1 class="font-weight-normal" style="color: #ec9a2e; margin-left: 80px;">One Set consists of:</h1>
+            <br><br>
+            <p class="lead"><span style="color: #23d4d4;"><i class="fas fa-circle mr-2"></i></i></span> 1 Frame made of PP with protector of EVA and eraser with width 10.5mm </p>
+            <p class="lead"><span style="color: #23d4d4;"><i class="fas fa-circle mr-2"></i></i></span> 5 Front Protective Windows that are 0.2mm thick and made of PET</p>
+        </div>
     </div>
 </div>
+<!-- GERY -->
 
 <div class="p-3 p-md-5 m-lg-3 text-center bg-light line-size">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 class="display-4 font-weight-normal">Faceshield</h1>
-        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+    <div class="row">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="font-weight-normal" style="color: #ec9a2e">Frame and Window</h1>
+            <br><br>
+            <p class="lead font-weight-normal">Extremely lightweight and elegant frame in which through a click system a reusable screen is installed. The frame is placed on the head with the help of an eraser that can be adjusted individually. For additional comfort a mellow protector for the forehead is placed. </p>
+        </div>
+        <div class="col-md-4">
+            <img src="../photo/camera_noback.png" alt="camera" style="height: 250px; margin-top: 65px;">
+        </div>
+    </div>
+</div>
+
+<!-- GERY -->
+<div class="p-3 p-md-5 m-lg-3 text-center bg-secondary line-size">
+    <div class="row">
+        <div class="col-md-4">
+            <img src="../photo/can_beer.png" alt="beer" style="height: 350px;">
+        </div>
+        <div class="col-md-5 p-lg-5 mx-auto my-5 text-white">
+            <h1 class="font-weight-normal" style="color: #ec9a2e">Easy window replacement </h1>
+            <br><br>
+            <p class="lead font-weight-normal">nTr Professional Faceshield has an innovative window fastening system that help you to easily replace the window anytime and anywhere without the help of any adjustments.</p>
+        </div>
+    </div>
+</div>
+<!-- GERY -->
+
+<div class="p-3 p-md-5 m-lg-3 text-center bg-light line-size">
+    <div class="row">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="font-weight-normal" style="color: #ec9a2e">Masks and Glasses</h1>
+            <br><br>
+            <p class="lead font-weight-normal">nTr Professional Faceshield is incredibly lightweight – only 46 g. it does not disturb the eye vision or the free movement of the head. It allows the usage of mask and glasses under the Faceshield and can be worn for hours without fatigue.</p>
+        </div>
+        <div class="col-md-4">
+            <img src="../photo/medicine.png" alt="medicine" style="height: 550px;">
+        </div>
+    </div>
+</div>
+
+<!-- GERY -->
+<div class="p-3 p-md-5 m-lg-3 bg-secondary line-size">
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <img src="../photo/new_logo_nonbackground.png" alt="logo" style="height: 250px; margin-top: 65px;">
+        </div>
+        <div class="col-md-5 p-lg-5 my-5 text-white" style="margin-left: 350px;">
+            <h1 class="font-weight-normal" style="color: #ec9a2e; margin-left: 80px;">Size and Adjustment </h1>
+            <br><br>
+            <p class="lead font-weight-normal">Easy adjustment of the eraser for a personalized and secure fit.</p>
+            <p class="lead"><span style="color: #23d4d4;"><i class="fas fa-circle mr-2"></i></i></span> Moderate size, suitable for most face shapes.</p>
+            <p class="lead"><span style="color: #23d4d4;"><i class="fas fa-circle mr-2"></i></i></span> Suitable for men and women.</p>
+        </div>
+    </div>
+</div>
+<!-- GERY -->
+
+<div class="p-3 p-md-5 m-lg-3 text-center bg-light line-size">
+    <div class="row">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="font-weight-normal" style="color: #ec9a2e">Disinfection and Storage </h1>
+            <br><br>
+            <p class="lead font-weight-normal">nTr Professional Faceshield – is not disinfected. Before usage disinfect the faceshield. Suitable for disinfection are all disinfectors containing ethanol. Avoid solvents, ketones, strong acids and alkaline elements.</p>
+        </div>
+        <div class="col-md-4">
+            <img src="../photo/bodyshop.jpg" alt="bodyshop" style="height: 350px;">
+        </div>
     </div>
 </div>
 @endsection
